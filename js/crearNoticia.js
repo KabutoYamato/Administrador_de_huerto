1413650847a1c4fcb0e28d7064147acd15c7869f
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-04-12 02:03:57 -0500

    Modulos
    *Falta Ver, tsss

--- js/crearNoticia.js
+++ js/crearNoticia.js
@@ -1,13 +1,9 @@
 
 var task;
 var postid;
-var postname;
-var postcientific;
-var postdescription;
-var posttaxonomy;
-var postaplications;
-var postqr;
 var posturl;
+var postname;
+var postdescription;
 var database = firebase.database().ref();
 var storage = firebase.storage().ref();
 var count;
@@ -15,37 +11,36 @@ var count;
 
 
 //Funcion para almacenar informacion en firebase///////////////////
-function WritePlanta(id, imageurl, name, cientific, description, taxonomy, aplications, qrcode){
-    database.child('informacion_plantas/' + id).set({
+function WriteNoticiaData(id, imageurl, name, description){
+    database.child('informacion_noticias/' + id).set({
+        descripcion: description,
         nombre: name,
-        cientifico: cientific,
-        descripcion: description,        
-        taxonomia: taxonomy,
-        aplicaciones: aplications,
-        qr: qrcode,
         url : imageurl
     });
     
     var updates = {};
-    updates['/plantas/' + postid] = true;
-    updates['/plantas/count'] = count + 1;
+    updates['/noticias/' + postid] = true;
+    updates['/noticias/count'] = count + 1;
+    updates['/noticias/Reciente/' + postid] = true
     var upd = database.update(updates);
-    upd.then((snapshot)=>{alert("Almacenado Correctamente en la Base de Datos")});
-    
+    upd.then((snapshot)=>{alert("Publicado Correctamente")});
 }
-/////////////////////////////////////////////////////////////////////
 
 //Funcion para obtener el contador siguiente del id ///////////////////
 function getNextKey(){
-    var zoneCount = database.child('plantas/count');
+    var zoneCount = database.child('noticias/count');
     zoneCount.once('value').then( 
         function(snapshot) {
             count = snapshot.val();
-            postid = 'UPQROO_P_'+(count+1);
-            postqr = 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data='+postid;
-            WritePlanta(postid, posturl, postname, postcientific, postdescription, posttaxonomy, postaplications, postqr);
+            postid = 'UPQROO_N_'+(count+1);
+            removeKey();
+            WriteNoticiaData(postid,posturl,postname,postdescription);
         }
     );
+}
+
+function removeKey(){
+    database.child('noticias/Reciente/UPQROO_N_' + (count - 1)).remove();
 }
 ///////////////////////////////////////////////////////////////////////
 
@@ -56,7 +51,7 @@ function saveImage(file){
     const metadata = {
         contentType: file.type
     };
-    task = storage.child("Plantas/"+name).put(file, metadata);
+    task = storage.child("Noticias/"+name).put(file, metadata);
     task.on('state_changed',
         function progress(snapshot){
             var percentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
@@ -79,16 +74,12 @@ function saveImage(file){
 }
 /////////////////////////////////////////////////////////////////////////////
 
-
 //Funcion del boton guardar
-$("#guardar").click(function(e){    
+$("#guardar").click(function(e){
     var file = $("#imagen").prop('files')[0];
     postname = $("#nombre").val();
-    postcientific = $("#cientifico").val();
     postdescription = $("#descripcion").val();
-    posttaxonomy = $("#taxonomia").val();
-    postaplications = $("#aplicaciones").val();
-    if(file != null && file != undefined && postname != "" && postdescription != "" && postcientific != "" && posttaxonomy != "" && postaplications != ""){
+    if(file != null && file != undefined && postname != "" && postdescription != ""){
         $('.modal').modal();
         saveImage(file);
     }
--- js/tarjeta.js
+++ js/tarjeta.js
@@ -6,16 +6,42 @@ function crearTarjetaPlanta(id,nombre,descripcion,url){
         '<div class="card">' +
             '<div class="card-image">' +
                 '<img src="'+ url + '" height="100%">' +
-                '<a class="btn-floating halfway-fab waves-effect waves-light green darken-3"><i class="material-icons">mode_edit</i></a>' +
+                '' +
                 '<span class="card-title text-shadow"><strong>' +
                     nombre +
                 '</strong></span>' +
             '</div>'+
-            '<div class="card-content" style="height:20%;">' +
+            '<div class="card-content">' +
                 '<p>' + descripcion + '</p>'+
             '</div>' +
             '<div class="card-action">' +
-                '<a href="verPlantas.html?id='+id+'&name='+nombre+'">Ver plantas</a>' +
+                '<a href="verPlantas.html?id='+id+'&name='+nombre+'">Ver planta</a>' +
+            '</div>' +
+        '</div>' +
+    '</div>';
+
+    return html;
+}
+
+//'<a href="verPlantas.html?id='+id+'&name='+nombre+'">Ver planta</a>'
+
+// Noticias
+function crearTarjetaNoticias(id,nombre,descripcion,url){
+    var html = 
+    '<div id="tarjeta" class="col s12 m6 l4">' +
+        '<div class="card">' +
+            '<div class="card-image">' +
+                '<img src="'+ url + '" height="100%">' +
+                '' +
+                '<span class="card-title text-shadow"><strong>' +
+                    nombre +
+                '</strong></span>' +
+            '</div>'+
+            '<div class="card-content">' +
+                '<p>' + descripcion + '</p>'+
+            '</div>' +
+            '<div class="card-action">' +
+                '<a href="verPlantas.html?id='+id+'&name='+nombre+'">Ver Noticia</a>' +
             '</div>' +
         '</div>' +
     '</div>';
